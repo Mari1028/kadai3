8c9b1f3fbd110ab16a00dcef4d02ce0f2fa37c8f
ohara <b1911mo@aiit.ac.jp>
2019-06-08 18:22:27 +0900

    内容修正
    まだ途中です・・

--- test.sh
+++ test.sh
@@ -2,3 +2,11 @@
 
 echo "file:" test -f /home/ohara/kadai/kadai3/kadai3.sh ; echo $?
 
+#seikai
+echo yes $1 $2 | awk '{print $1/NR RS $2/NR}'| grep -Fv --line-buffered . | awk 'a[$1]++{print;exit}' > /tmp/$$-answer
+
+#kekka
+./kadai3.sh > /tmp/$$-result
+
+# Test
+diff /tmp/$$-answer /tmp/44-result && echo "OK" || echo "ERROR"
